82554d7b901749e0a36e5cecd23751002b979d67
Carlos Tomé Cortiñas <carlos.tome-cortinas@iohk.io>
2025-10-10 15:04:13 +0200

    Use flakes and agda.nix (#15)
    * Use agda.nix
    
    * Add hydraJobs
    
    * Change supported systems
    
    * Update nixpkgs
    
    * Compose overlay with imported overlays

---
+++ flake.nix
@@ -0,0 +1,80 @@
+{
+  description = "abstract-set-theory";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    shellFor = {
+      url = "github:input-output-hk/agda.nix?dir=tools/shellFor";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    standard-library-classes = {
+      url = "github:input-output-hk/agda.nix?dir=libraries/standard-library-classes";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    standard-library-meta = {
+      url = "github:input-output-hk/agda.nix?dir=libraries/standard-library-meta";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.standard-library-classes.follows = "standard-library-classes";
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      overlay = nixpkgs.lib.composeManyExtensions [
+        inputs.standard-library-classes.overlays.default
+        inputs.standard-library-meta.overlays.default
+        (final: prev: {
+          agdaPackages = prev.agdaPackages.overrideScope (
+            afinal: aprev: {
+              abstract-set-theory = afinal.callPackage ./nix/abstract-set-theory.nix { };
+            }
+          );
+        })
+      ];
+    in
+    flake-utils.lib.eachSystem systems (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.shellFor.overlays.default
+            overlay
+          ];
+        };
+      in
+      {
+        packages.default = pkgs.agdaPackages.abstract-set-theory;
+        devShells.default = pkgs.agda.shellFor pkgs.agdaPackages.abstract-set-theory;
+        hydraJobs =
+          let
+            jobs = { inherit (self) packages devShells; };
+          in
+          jobs
+          // {
+            required = pkgs.releaseTools.aggregate {
+              name = "${system}-required";
+              constituents = with nixpkgs.lib; collect isDerivation jobs;
+            };
+          };
+      }
+    )
+    // {
+      overlays.default = overlay;
+    };
+}
